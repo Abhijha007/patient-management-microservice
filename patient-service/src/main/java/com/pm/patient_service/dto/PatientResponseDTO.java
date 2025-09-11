9cde9a9fba0e8746038e5e01e419663fc2fdef17
Abhijha007 <arastujha@gmail.com>
2025-09-12 03:00:06 +0530

    init commit

--- patient-service/src/main/java/com/pm/patient_service/dto/PatientResponseDTO.java
+++ patient-service/src/main/java/com/pm/patient_service/dto/PatientResponseDTO.java
@@ -1,4 +1,9 @@
 package com.pm.patient_service.dto;
 
 public class PatientResponseDTO {
+    private String id;
+    private String name;
+    private String email;
+    private String address;
+    private String dateOfBirth;
 }
