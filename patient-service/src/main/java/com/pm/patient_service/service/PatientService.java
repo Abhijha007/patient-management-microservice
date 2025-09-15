7568b90f0122e9f2651198b8a7eb91a880f02846
Abhijha007 <arastujha@gmail.com>
2025-09-15 21:00:17 +0530

    init commit

--- patient-service/src/main/java/com/pm/patient_service/service/PatientService.java
+++ patient-service/src/main/java/com/pm/patient_service/service/PatientService.java
@@ -1,6 +1,7 @@
 package com.pm.patient_service.service;
 
 import com.pm.patient_service.dto.PatientResponseDTO;
+import com.pm.patient_service.mapper.PatientMapper;
 import com.pm.patient_service.model.Patient;
 import com.pm.patient_service.repository.PatientRepository;
 import org.springframework.stereotype.Service;
@@ -10,7 +11,7 @@ import java.util.List;
 @Service
 public class PatientService {
 
-    private PatientRepository patientRepository;
+    private final PatientRepository patientRepository;
 
     public PatientService (PatientRepository patientRepository){
         this.patientRepository = patientRepository;
@@ -19,6 +20,7 @@ public class PatientService {
     public List<PatientResponseDTO> getPatients(){
         List<Patient> patients = patientRepository.findAll();
 
-        return null;
+        return patients.stream()
+                .map(PatientMapper::toDTO).toList();
     }
 }
