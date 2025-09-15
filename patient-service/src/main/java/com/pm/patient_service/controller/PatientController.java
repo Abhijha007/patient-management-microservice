ae9fb65fc11128bc277982df72550bd635fe26b7
Abhijha007 <arastujha@gmail.com>
2025-09-15 21:01:34 +0530

    init commit

--- patient-service/src/main/java/com/pm/patient_service/controller/PatientController.java
+++ patient-service/src/main/java/com/pm/patient_service/controller/PatientController.java
@@ -1,4 +1,9 @@
 package com.pm.patient_service.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/patients")
 public class PatientController {
 }
