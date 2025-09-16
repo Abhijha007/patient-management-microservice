631b24c272b56a5d0c1e66e90ab9d61c58b93739
Abhijha007 <arastujha@gmail.com>
2025-09-16 20:26:24 +0530

    init commit

--- patient-service/src/main/java/com/pm/patient_service/controller/PatientController.java
+++ patient-service/src/main/java/com/pm/patient_service/controller/PatientController.java
@@ -22,6 +22,6 @@ public class PatientController {
     @GetMapping
     public ResponseEntity<List<PatientResponseDTO>> getPatients(){
         List<PatientResponseDTO> patients = patientService.getPatients();
-        return null;
+        return ResponseEntity.ok().body(patients);
     }
 }
