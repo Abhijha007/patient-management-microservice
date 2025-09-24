43f8389a24f451e9a61e192e26ff61077a6918d4
Abhijha007 <arastujha@gmail.com>
2025-09-24 22:37:38 +0530

    init commit

--- patient-service/src/main/java/com/pm/patient_service/exception/GlobalExceptionHandler.java
+++ patient-service/src/main/java/com/pm/patient_service/exception/GlobalExceptionHandler.java
@@ -1,7 +1,19 @@
 package com.pm.patient_service.exception;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.Map;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationException(
+            MethodArgumentNotValidException ex
+    ){
+
+        return null;
+    }
 }
