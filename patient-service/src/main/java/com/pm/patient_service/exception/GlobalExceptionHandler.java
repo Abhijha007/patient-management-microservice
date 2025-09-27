3deee18b7dbad78c740a5d8ecee152f44dd67076
Abhijha007 <arastujha@gmail.com>
2025-09-27 21:05:52 +0530

    init commit

--- patient-service/src/main/java/com/pm/patient_service/exception/GlobalExceptionHandler.java
+++ patient-service/src/main/java/com/pm/patient_service/exception/GlobalExceptionHandler.java
@@ -21,4 +21,13 @@ public class GlobalExceptionHandler {
 
         return ResponseEntity.badRequest().body(errors);
     }
+
+    @ExceptionHandler(EmailAlreadyExistsException.class)
+    public ResponseEntity<Map<String, String>>
+    handleEmailAlreadyExistsException(EmailAlreadyExistsException ex){
+
+        Map<String, String> errors = new HashMap<>();
+
+        return null;
+    }
 }
